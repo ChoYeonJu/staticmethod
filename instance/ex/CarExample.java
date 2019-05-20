0226ad78a91cc8b421f3aacdab3911bb638c298d
ChoYeonJu <yjjoh@gmail.com>
2019-05-20 13:29:52 +0900

    Add instance

---
+++ instance/ex/CarExample.java
@@ -0,0 +1,12 @@
+package instance.ex;
+
+public class CarExample {
+
+	public void main(String[] args) {
+		Car myCar = new Car("Æ÷¸£½¦");
+		Car yourCar = new Car("º¥Ã÷");
+		
+		myCar.run();
+		yourCar.run();
+	}
+}
